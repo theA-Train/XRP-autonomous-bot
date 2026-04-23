5e4a398abf95f411c5a52a4dd70656d2e6b46c41
theA-Train <146664346+theA-Train@users.noreply.github.com>
2026-04-23 09:10:53 -0400

    init gyro

--- robot.py
+++ robot.py
@@ -2,6 +2,8 @@ import os
 import wpilib
 from wpilib import Timer
 from xrp import XRPMotor
+from xrp import XRPGyro
+from math import pi
 
 os.environ["HALSIMXRP_HOST"] = "192.168.42.1"
 os.environ["HALSIMXRP_PORT"] = "3540" #3540
@@ -13,26 +15,15 @@ class MyRobot(wpilib.TimedRobot):
 		self.left_motor = XRPMotor(0)
 		self.right_motor = XRPMotor(1)
 		self.right_motor.setInverted(True) 
+		self.gyro = XRPGyro()
 
 	def autonomousInit(self):
 		print("autonomousInit")
-		self.now = Timer()
-		self.now.reset()
-		self.now.start()
+
 
 	def autonomousPeriodic(self):
 		print("autonomousPeriodic")
-		# Move forward (speed, rotation) for 2 seconds
-		print(self.now.get())
-		for i in range(3):
-			time_iteration = 0
-			if self.now.get() + time_iteration < 2:
-				self.left_motor.set(0.5)
-				self.right_motor.set(0.5)
-			elif 2 < self.now.get() + time_iteration > 3: 
-				self.right_motor.set(0.5)
-				self.left_motor.stopMotor()
-				time_iteration = 2
+		print(self.gyro.getAngle() * (180/pi))
 		
 if __name__ == "__main__":
 	wpilib.run(MyRobot)
