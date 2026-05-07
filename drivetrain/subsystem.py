b1991e785315ff50cea37ec5432deefd904950d2
theA-Train <146664346+theA-Train@users.noreply.github.com>
2026-05-07 17:31:43 -0400

    Update subsystem.py

--- drivetrain/subsystem.py
+++ drivetrain/subsystem.py
@@ -1,4 +1,5 @@
 from wpilib import Encoder
+import wpimath
 from commands2 import Subsystem
 from xrp import XRPMotor, XRPGyro
 import math
@@ -8,11 +9,8 @@ class Drivetrain(Subsystem):
         self.left_motor = XRPMotor(0)
         self.right_motor = XRPMotor(1)
         self.max_effort = 0.8
-        self.min_effort = 0.0
+        self.min_effort = -0.8
 
-        self.max_angle = 360
-        self.min_angle = 0.0
-		
         self.right_motor.setInverted(True) 
         
         self.left_encoder = Encoder(4,5)
@@ -20,14 +18,18 @@ class Drivetrain(Subsystem):
         self.gyro = XRPGyro()
 
     def get_gyro_angle(self):
-        return self.gyro.getAngle() * (180/math.pi)
+        # return self.gyro.getRotation2d().degrees() - 360 * math.floor((self.gyro.getRotation2d().degrees() + 180) / 360) this is essentially what angle modulus is doing
+        return wpimath.angleModulus(self.gyro.getAngle()) * (180/math.pi)
     
     def clamp_motor_values(self, value: float) -> float:
         return max(min(value, self.max_effort),self.min_effort)
     
-    def clamp_gyro_values(self, value: float) -> float:
-        return max(min(value, self.max_angle), self.min_angle)
+    def set_left_motor(self, value:float):
+        self.left_motor.set(self.clamp_motor_values(value))
 
+    def set_right_motor(self, value:float):
+        self.right_motor.set(self.clamp_motor_values(value))
+    
 
 
 
