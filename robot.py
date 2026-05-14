5d47c1c9a907aa8b9b4090704411bcf11193ccea
theA-Train <146664346+theA-Train@users.noreply.github.com>
2026-05-14 08:52:48 -0400

    Revert "diffy drive and tuning"
    This reverts commit d078ed2c1487f7948ccf1f29bb7efd910e0019b4.

--- robot.py
+++ robot.py
@@ -15,11 +15,22 @@ class MyRobot(wpilib.TimedRobot):
 		self.drivetrain = Drivetrain()
 
 	def autonomousInit(self):
+		self.drivetrain.right_encoder.reset()
+		self.drivetrain.left_encoder.reset()
 
-		self.line = Drive_To_Distance(self.drivetrain, 5000, 0.4)
-		self.angle = Rotate_Drivetrain(self.drivetrain, 60) # change the angle to get the desired equilateral shape :)
-		self.shape = Shape_Path(self.line, self.angle).repeatedly()
-		self.shape.schedule()
+		# initializing all the commands to run the ramp routine
+		self.drive_to_ramp_command = Drive_To_Distance(self.drivetrain, 5500, 0.4)
+		self.drive_up_ramp_command = Drive_To_Distance(self.drivetrain, 34300, 0.7)
+		self.rotate_to_ramp_command = Rotate_Drivetrain(self.drivetrain, 85)
+		self.rotate_from_ramp_command = Rotate_Drivetrain(self.drivetrain, 88)
+		self.drive_bridge_command = Drive_To_Distance(self.drivetrain, 20000, 0.4)
+		self.wait_command = Wait(0.3)
+
+		# the ramp_routine class passes in all the commands initalized above into a "sequential command group" that runs one by one
+		self.ramp_auto = Ramp_Routine(self.drive_to_ramp_command, self.wait_command, self.rotate_to_ramp_command, self.drive_up_ramp_command, self.rotate_from_ramp_command, self.drive_bridge_command)
+		# self.rotate_to_ramp_command.schedule()
+		self.ramp_auto.schedule()
+		# self.drive_up_ramp_command.schedule()
 		print("autonomousInit")
 
 	def autonomousPeriodic(self):
